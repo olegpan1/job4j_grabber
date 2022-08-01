a750e1f77b12344deef697414e8dea7a75055515
olegpan1 <olegpan@hotmail.com>
2022-08-01 15:28:22 +0700

    2. Парсинг HTML страницы. [#260358 #198311]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -22,7 +22,8 @@ public class HabrCareerParse {
             Element titleElement = row.select(".vacancy-card__title").first();
             Element linkElement = titleElement.child(0);
             String vacancyName = titleElement.text();
-            String date = row.select(".vacancy-card__date").text();
+            String date =  row.select(".vacancy-card__date")
+                    .first().child(0).attr("datetime").split("T")[0];
             String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
             System.out.printf("%s %s%n %s%n%n", date, vacancyName, link);
         });
