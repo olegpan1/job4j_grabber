37f5741dcc5d0c277d76d47c2b7619a782a3d52d
olegpan1 <olegpan@hotmail.com>
2022-08-01 12:24:53 +0700

    2. Парсинг HTML страницы. [#260358 #198311]

--- src/main/java/ru/job4j/quartz/QuartzTest.java
+++ src/main/java/ru/job4j/quartz/QuartzTest.java
@@ -5,26 +5,23 @@ import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
 import org.quartz.impl.StdSchedulerFactory;
-import static org.quartz.JobBuilder.*;
-import static org.quartz.TriggerBuilder.*;
-import static org.quartz.SimpleScheduleBuilder.*;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 public class QuartzTest {
 
     public static void main(String[] args) {
 
         try {
-            // Grab the Scheduler instance from the Factory
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
 
-            // and start it off
             scheduler.start();
-// define the job and tie it to our HelloJob class
             JobDetail job = newJob(HelloJob.class)
                     .withIdentity("job1", "group1")
                     .build();
 
-            // Trigger the job to run now, and then repeat every 40 seconds
             Trigger trigger = newTrigger()
                     .withIdentity("trigger1", "group1")
                     .startNow()
@@ -33,7 +30,6 @@ public class QuartzTest {
                             .repeatForever())
                     .build();
 
-            // Tell quartz to schedule the job using our trigger
             scheduler.scheduleJob(job, trigger);
             Thread.sleep(20000);
             scheduler.shutdown();
