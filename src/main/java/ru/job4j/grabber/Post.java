26cd0c7eaa5566473b1526210dfa6921d3a87d52
olegpan1 <olegpan@hotmail.com>
2022-08-02 14:45:40 +0700

    2.2. Модель данных - Post. [#285211 #198752]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -55,15 +55,12 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return id == post.id
-                && Objects.equals(title, post.title)
-                && Objects.equals(link, post.link)
-                && Objects.equals(created, post.created);
+        return Objects.equals(id, post.id) && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, link, created);
+        return Objects.hash(id, link);
     }
 
     @Override
