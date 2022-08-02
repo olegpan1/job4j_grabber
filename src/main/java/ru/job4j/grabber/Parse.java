22e01106ef6738f7875c6552eace4f07b48a40f8
olegpan1 <olegpan@hotmail.com>
2022-08-02 18:33:25 +0700

    2.4. HabrCareerParse [#285213 #200606]

--- src/main/java/ru/job4j/grabber/Parse.java
+++ src/main/java/ru/job4j/grabber/Parse.java
@@ -5,6 +5,4 @@ import java.util.List;
 public interface Parse {
 
     List<Post> list(String link);
-
-    Post detail(String link);
 }
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -56,7 +56,6 @@ public class SqlRuParse implements Parse {
         return elements;
     }
 
-    @Override
     public Post detail(String link) {
         Element parent = loadPostDetails(link);
         return new Post(0,
